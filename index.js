2a18d2c65ea0cf686675644fbade159b9ef04a00
Nathalia <nathidomingos1305@gmail.com>
2026-05-26 16:39:10 -0300

    Implement validarEntrada function for input validation
    Add input validation function for user data.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+Function validarEntrada(nome, idade, animal, vip){
 
+if(nome === ""){
+  return "Erro 400, Precisa ter um nome!!";
+}
+if(idade > 20 || idade < 0){
+return "Erro 400, idade invalida";
+}
+
+if(animal === "Gato" && vip === false){
+  return "Erro 400, Seu animal não é um gato";
+  ]
+}
+Return"Sucesso 201";
+}
